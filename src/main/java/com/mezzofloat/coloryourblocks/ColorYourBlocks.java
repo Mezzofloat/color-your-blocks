4780d4e753cb43440cba4bc48349beb5696b24ca
Mezzofloat <skull.wizard.2299@gmail.com>
2024-08-31 11:03:37 -0400

    Initial commit

---
+++ src/main/java/com/mezzofloat/coloryourblocks/ColorYourBlocks.java
@@ -0,0 +1,17 @@
+package com.mezzofloat.coloryourblocks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ColorYourBlocks implements ModInitializer {
+	public static final String MOD_ID = "color-your-blocks";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/mezzofloat/coloryourblocks/ColorYourBlocksClient.java
@@ -0,0 +1,10 @@
+package com.mezzofloat.coloryourblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColorYourBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
