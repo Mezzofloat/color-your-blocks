f52a6b7dc5abef99a76f8dbac7e854d7b649e7bc
Mezzofloat <skull.wizard.2299@gmail.com>
2025-01-14 00:21:00 -0500

    added outlining commentary

---
+++ mergeresourcepack.py
@@ -0,0 +1,7 @@
+# search for resource paack from last time (should be prefixed with . or /)
+
+# iterate over every image
+# each pixel should be the previous + the new
+# merge into a new resource pack
+
+# rename the new resource pack to [previous]+[new]
