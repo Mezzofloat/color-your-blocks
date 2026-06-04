edc015013f109016ae19dd6d42264883fe0cf76c
Mezzofloat <skull.wizard.2299@gmail.com>
2026-06-04 15:25:24 -0400

    changed merge resource pack to use full_subset rather than full

--- mergeresourcepack.py
+++ mergeresourcepack.py
@@ -23,10 +23,9 @@ minecraftcolors = {
 }
 
 # define needed paths
-full_pack_path = Path("full")
+full_pack_path = Path("full_subset")
 resource_pack_path = Path("color-your-blocks")
 mask_pack_path = Path("masks")
-test_image_path = Path("test\\assets\\minecraft\\textures\\tumblr_8e62b2cb31fbb752647650f0191ee12a_c83283a0_1280.webp")
 
 all_white_path = Path("white")
 
